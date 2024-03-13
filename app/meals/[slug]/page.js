959544fc3732d14cea71f1d95487fe6478443f77
laszczykjakub <jakub.l@gqg.pl>
2024-03-13 23:04:12 +0100

    add structure and data to meal details page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,30 @@
-export default function MealPage({params}){
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default function MealDetailsPage({params}){
+    const meal = getMeal(params.slug);
+
+    meal.instructions = meal.instructions.replace(/\n/g,'<br/>');
     return (
+        <>
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image src={meal.image} alt={meal.title} fill/>
+            </div>
+            <div className={classes.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={classes.creator}>
+                    by <a href={`mailto:${meal.email}`}>{meal.creator}</a>
+                </p>
+                <p className={classes.summary}>{meal.summary}</p>
+            </div>
+        </header>
         <main>
-            <h1>Meal Page</h1>
-            <p>{params.slug}</p>
+            <p className={classes.instructions} dangerouslySetInnerHTML={{
+                __html:meal.instructions,
+            }}></p>
         </main>
+        </>
     )
 }
